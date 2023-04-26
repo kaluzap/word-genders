cfbf63d77d9b791c342b519798391e446d0ece27
kaluzap <pablokaluza@gmail.com>
2023-04-26 23:43:05 +0200

    Adding nouns tool frame

--- words.py
+++ words.py
@@ -9,7 +9,18 @@ class App(tk.Tk):
         self.geometry("550x500")
         self.protocol("WM_DELETE_WINDOW", self.close_window)
         self.resizable(False, False)
-
+        self._frame = None
+        self.ControlFrame = ControlFrame
+        self.switch_frame(ControlFrame)
+    
+    def switch_frame(self, frame_class):
+        """Destroys current frame and replaces it with a new one."""
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+        
     def close_window(self):
         print("Ciao")
         quit()
@@ -17,7 +28,7 @@ class App(tk.Tk):
         
 def main():
     app = App()
-    ControlFrame(app)
+    #ControlFrame(app)
     app.mainloop()
     
 
